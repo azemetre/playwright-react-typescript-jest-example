99b8ec0adc5cfaca5c8dc1dbfa9bf6cdc4629c8e
Aaron Zemetres <azemetre@gmail.com>
2020-02-23 15:15:44 -0500

    test: defining envs in tests

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -1,17 +1,6 @@
-import { chromium, firefox, webkit, devices } from 'playwright';
-
-const iPhone = devices['iPhone 6'];
-const iPad = devices['iPad Pro 11'];
-const galaxyNote = deivces['Galaxy Note 3'];
-
 module.exports = {
-  browserType: ["chromium", "firefox", "webkit"],
   launchConfig: {
     headless: false,
     slowMo: 10,
   },
-  contextConfig: {
-    viewport: iPhone.viewport,
-    userAgent: iPhone.userAgent,
-  }
 };
