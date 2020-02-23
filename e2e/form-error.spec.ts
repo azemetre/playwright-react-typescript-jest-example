ceedb677434432fc8f4c887e0ce7a7c4009dfe55
Aaron Zemetres <azemetre@gmail.com>
2020-02-23 14:10:26 -0500

    test: base scenarios

---
+++ e2e/form-error.spec.ts
@@ -0,0 +1,33 @@
+const playwright = require("playwright");
+
+describe("Form Tests - all Browsers", () => {
+  it("Unable to Submit Empty Form", async () => {
+    // running tests on all 3 browsers
+    for (const browserType of ["chromium", "firefox", "webkit"]) {
+      const browser = await playwright[browserType].launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto("http://localhost:8080/");
+
+      // screenshot prior to error
+      await page.screenshot({
+        path: `e2e/screenshots/form-error-not-visible-${browserType}-${new Date().valueOf()}.png`
+      });
+
+      // action to trigger form error
+      await page.click("css=button");
+
+      // defining selectors
+      const errorMsg = await page.$eval("css=p", el => el.textContent);
+
+      // verifying elements exist
+      expect(errorMsg).toEqual("a username is required");
+
+      // screenshot of error
+      await page.screenshot({
+        path: `e2e/screenshots/form-error-visible-${browserType}-${new Date().valueOf()}.png`
+      });
+      await browser.close();
+    }
+  }, 15000);
+});
---
+++ e2e/form-success.spec.ts
@@ -0,0 +1,38 @@
+const playwright = require("playwright");
+
+describe("Form Tests - all Browsers", () => {
+  it("Unable to Submit Empty Form", async () => {
+    // running tests on all 3 browsers
+    for (const browserType of ["chromium", "firefox", "webkit"]) {
+      const browser = await playwright[browserType].launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto("http://localhost:8080/");
+
+      // screenshot prior to submit
+      await page.screenshot({
+        path: `e2e/screenshots/form-before-submit-${browserType}-${new Date().valueOf()}.png`
+      });
+
+      const h1 = await page.$eval("css=h1", el => el.textContent);
+      expect(h1).toEqual('Please Submit the Form');
+
+      // evi nemeth was an expert in system admin and wrote many famous books
+      // https://en.wikipedia.org/wiki/Evi_Nemeth
+      const user = "evi.nemeth";
+
+      await page.fill("css=input", user);
+      await page.click("css=button");
+
+      // screenshot of successful submit
+      await page.screenshot({
+        path: `e2e/screenshots/form-after-submit-${browserType}-${new Date().valueOf()}.png`
+      });
+
+      // previous h1 is a stale element, will need to re-assert the element
+      expect(await page.$eval("css=h1", el => el.textContent)).toEqual('Thank you for Submitting');
+
+      await browser.close();
+    }
+  }, 15000);
+});
