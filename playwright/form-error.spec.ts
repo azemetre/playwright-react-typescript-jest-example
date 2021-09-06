24396e166fa7af51cbf90941684f280666a6b9d9
Murat Ozcan <muratkerem@gmail.com>
2021-09-06 09:08:41 -0500

    introduced wait-on

--- playwright/form-error.spec.ts
+++ playwright/form-error.spec.ts
@@ -3,25 +3,21 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Form Tests', () => {
   test('Unable to Submit Empty Form', async () => {
-    const browserList = ['chromium'];
+    const browser = await playwright.chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://localhost:9091/');
 
-    for (const browserType of browserList) {
-      const browser = await playwright[browserType].launch({ headless: false });
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      await page.goto('http://localhost:9091/');
+    await page.click('css=button');
 
-      await page.click('css=button');
+    const errorMsg = await page.$eval('css=p', el => el.textContent);
 
-      const errorMsg = await page.$eval('css=p', el => el.textContent);
+    expect(errorMsg).toEqual('a username is required');
 
-      expect(errorMsg).toEqual('a username is required');
+    await page.screenshot({
+      path: `playwright/screenshots/form-error-visible-${new Date().valueOf()}.png`
+    });
 
-      await page.screenshot({
-        path: `playwright/screenshots/form-error-visible-${browserType}-${new Date().valueOf()}.png`
-      });
-
-      await browser.close();
-    }
+    await browser.close();
   });
 });
--- playwright/form-success.spec.ts
+++ playwright/form-success.spec.ts
@@ -3,23 +3,23 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Form Tests', () => {
   test('Can submit a form when filled correctly', async () => {
-      const browser = await playwright.chromium.launch({ headless: false });
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      await page.goto('http://localhost:9091/');
+    const browser = await playwright.chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://localhost:9091/');
 
-      const h1 = await page.$eval('css=h1', el => el.textContent);
-      expect(h1).toEqual('Please Submit the Form');
+    const h1 = await page.$eval('css=h1', el => el.textContent);
+    expect(h1).toEqual('Please Submit the Form');
 
-      await page.fill('css=input', 'evi.nemeth');
-      await page.click('css=button');
+    await page.fill('css=input', 'evi.nemeth');
+    await page.click('css=button');
 
-      expect(await page.$eval('css=h1', el => el.textContent)).toEqual('Thank you for Submitting');
+    expect(await page.$eval('css=h1', el => el.textContent)).toEqual('Thank you for Submitting');
 
-      await page.screenshot({
-        path: `playwright/screenshots/form-after-submit-${new Date().valueOf()}.png`
-      });
+    await page.screenshot({
+      path: `playwright/screenshots/form-after-submit-${new Date().valueOf()}.png`
+    });
 
-      await browser.close();
+    await browser.close();
   });
 });
